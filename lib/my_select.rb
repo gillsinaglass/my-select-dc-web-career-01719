dab7730a2abea979b6b62d278ea2fb9f19a011b7
Ryan Gill <rdgill95@gmail.com>
2018-12-31 17:35:18 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,10 @@
 def my_select(collection)
- 
+  i = 0 
+  empty_array = []
+  while i < collection.length 
+    if yield(collection[i]) == true 
+      empty_array << collection[i]
+    end
+  i += 1 
+  end 
 end
