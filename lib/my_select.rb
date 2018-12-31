d4f83358ded7e6ac83dee84b9dd82097fff08777
Ryan Gill <rdgill95@gmail.com>
2018-12-31 17:36:52 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,7 @@ def my_select(collection)
   i = 0 
   empty_array = []
   while i < empty_array.length 
-    if yield(collection[i]) == true 
+    if yield(empty_array[i]) == true 
       empty_array << collection[i]
     end
   i += 1 
